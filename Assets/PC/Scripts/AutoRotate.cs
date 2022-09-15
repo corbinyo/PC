bb70c2a0134e6fbb2470bde229a3880345aaf3de
corbinyo <corbinordel@gmail.com>
2022-09-14 23:17:24 -0400

    2nd wheel and sequencer added and labeling

--- Assets/PC/Scripts/AutoRotate.cs
+++ Assets/PC/Scripts/AutoRotate.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit.UI;
-
 using Photon.Pun;
 using Photon.Realtime;
 public class AutoRotate : MonoBehaviour
 {
     public float Speed;
-
     public bool RotateOnX = true;
     public bool RotateOnY = true;
     public bool RotateOnZ = true;
@@ -17,29 +15,31 @@ public class AutoRotate : MonoBehaviour
     public bool play = true;
     void Start()
     {
-        myPV = GetComponent<PhotonView>();
+       myPV = GetComponent<PhotonView>();
        
-      pinchSlider = myPV.GetComponent<AutoRotate>().pinchSlider;
+      //pinchSlider = myPV.GetComponent<AutoRotate>().pinchSlider;
 
 
     }
     [PunRPC]
     void SpeedAdjust(float speed)
     {
-        Speed = speed;
-        Debug.Log("the speed is : " + speed);
-        if (!myPV.IsMine)
-        {
-            pinchSlider.SliderValue = speed;
-        }
+        //Speed = speed;
+        //Debug.Log("the speed is : " + speed);
+        //if (!myPV.IsMine)
+        //{
+        //    pinchSlider.SliderValue = speed;
+        //}
 
     }
     
     public void OnSliderUpdated(SliderEventData eventData)
     {
-        // float current = float.Parse($"{eventData.NewValue:F2}") * 100;
-        float current = float.Parse($"{eventData.NewValue:F2}");
-        myPV.RPC("SpeedAdjust", RpcTarget.All, current);
+
+        Speed = float.Parse($"{eventData.NewValue:F2}") * 100f;
+
+
+       // myPV.RPC("SpeedAdjust", RpcTarget.All, eventData);
        
         
     }
@@ -47,9 +47,10 @@ public class AutoRotate : MonoBehaviour
 
     void Update()
     {
+
         if (play == true)
         {
-            Vector3 rotFactor = Vector3.one * Speed * 100;
+            Vector3 rotFactor = Vector3.one * Speed;
 
             if (!RotateOnX) rotFactor.x = 0;
             if (!RotateOnY) rotFactor.y = 0;
